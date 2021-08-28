23d961ea249e21faa785e40a2f9b1b50388af64d
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2021-08-28 09:36:16 -0500

    dojo done

---
+++ App.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+app.secret_key = 'a_secret_key'
+
+
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        print(fullname)
+        print(phone)
+        print(email)
+        flash('Contact added successfully')
+        return redirect(url_for('index'))
+
+
+@app.route('/edit_contact')
+def edit_contact():
+    return 'edit'
+
+
+@app.route('/delete_contact')
+def delete_contact():
+    return 'delete'
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
